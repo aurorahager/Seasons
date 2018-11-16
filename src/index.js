8adf65cecae26409d0d0bfb4159cfba2b5916932
Rory Hager <roryy18@gmail.com>
2018-11-16 17:32:42 -0600

    update state with geolocation

--- src/index.js
+++ src/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    // init state
+    this.state = { lat: null };
+
     // get user's current location
     window.navigator.geolocation.getCurrentPosition(
-      //success callback
-      position => console.log(position),
+      //success callback, set lat in state to user's location
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
       //failure callback
       err => console.log(err)
-    );
-    return <div>Latitude: </div>;
-  }
-}
+    ); //END geolocation
+  } //END constructor
+
+  render() {
+    return <div>{this.state.lat}</div>;
+  } //END render
+} //END App component
 
 ReactDOM.render(<App />, document.querySelector('#root'));
