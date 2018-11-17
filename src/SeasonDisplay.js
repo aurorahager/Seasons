455b8c1e0df7388b168f1f7d17b105650ead59ac
Rory Hager <roryy18@gmail.com>
2018-11-17 16:14:34 -0600

    getting season logic

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
-};
+// function to get the current season base on latitude
+const getSeason = (lat, month) => {
+  // if between march and september
+  if (month > 2 && month < 9) {
+    // northern hemisphere summer, southern winter
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    // not those months: winter northern hemisphere,summer souther
+    return lat > 0 ? 'winter' : 'summmer';
+  } // END if/else
+}; // END getSeason
+
+// component
+const SeasonDisplay = props => {
+  // get season with users lat and current month
+  const season = getSeason(props.lat, new Date().getMonth());
+  // assign text and icon depending on season
+  const text =
+    season === 'winter' ? 'burr, it is chilly' : 'lets hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  // render JSX
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  ); // END return
+}; // END SeasonDisplay component
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  // One line way to init state without constructor
   // THIS IS THE ONLY TIME we do a direct assignment to this.state
   state = { lat: null, errorMessage: '' };
 
-  /* API calls and data loading should be in life cycle methods (best practice
-  not to do in constructor) not in the render method. The DOM will re-render
-  with each state change therefor we want these things outside the render as
-  to not preform multiple API calls (for each rendering) or come across errors*/
-
+  // Get user's current location on mount
   componentDidMount() {
-    // get user's current location
     window.navigator.geolocation.getCurrentPosition(
-      //success callback, set lat in state to user's location
-      // set state (do not reassign this.state)
+      //success callback
       position => this.setState({ lat: position.coords.latitude }),
       //failure callback
       err => this.setState({ errorMessage: err.message })
     ); //END geolocation
   } //END didMount
 
-  /* React requires a render method in every class component.
-  render is ONLY for rendering JXS */
   render() {
-    //if error render error message
+    //if error render, error message
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error: {this.state.errorMessage}</div>;
     }
-    //if lat is given and no error render lat
+    //if lat is given and no error, render lat
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    //if not above (ie no data) render loading
+    //if not above (ie no data), render loading
     return <div>Loading...</div>;
   } //END render
 } //END App component
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+/////////////////////////////////////////////////////////////////////////
+//                            NOTES
+////////////////////////////////////////////////////////////////////////
+
+/* React requires a render method in every class component.
+render is ONLY for rendering JXS */
 
 /*
 !How to initialize the state in the constructor
@@ -50,6 +49,10 @@ ReactDOM.render(<App />, document.querySelector('#root'));
 */
 
 /*
+API calls and data loading should be in life cycle methods (best practice
+not to do in constructor) not in the render method. The DOM will re-render
+with each state change therefor we want these things outside the render as
+to not preform multiple API calls (for each rendering) or come across errors
 ! Life cycle methods:
 *componentDidMount(){}
 - will run code once when component first loads
@@ -60,12 +63,12 @@ ReactDOM.render(<App />, document.querySelector('#root'));
 *componentWillUnmount(){}
 - will wait till component is on longer shown then will run code
 
-? Less common methods
-componentDidCatch
-componentWillReceiveProps
-componentWillUpdate
-shouldComponentUpdate
-componentWillMount
-getDerivedSateFromProps
-getSnapshotBeforeUpdate
+* Less common methods
+- componentDidCatch
+- componentWillReceiveProps
+- componentWillUpdate
+- shouldComponentUpdate
+- componentWillMount
+- getDerivedSateFromProps
+- getSnapshotBeforeUpdate
 */
